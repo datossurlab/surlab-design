77c1c7b3032cd71e475fb1e4c3de2a807ff5397b
datos-surlab <surlabinnovacionpolitica@gmail.com>
2026-08-19 18:23:29 -0300

    Create page.tsx

---
+++ app/jornadas2026/page.tsx
@@ -0,0 +1,8 @@
+export default function Jornadas2026Page() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Jornadas 2026</h1>
+      <p>Contenido de las jornadas próximamente.</p>
+    </main>
+  );
+}
